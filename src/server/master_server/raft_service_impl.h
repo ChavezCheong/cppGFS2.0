fb7cf4303fe505f15905c1158ae5cf5e9d6dafd5
Mark Liu <markyangliu3@gmail.com>
2023-03-23 00:08:06 -0400

    Changed raft interface to raft service

--- src/server/master_server/raft_service_impl.h
+++ src/server/master_server/raft_service_impl.h
@@ -1,13 +1,17 @@
 #ifndef GFS_SERVER_MASTER_SERVER_RAFT_CONSENSUS_H_
 #define GFS_SERVER_MASTER_SERVER_RAFT_CONSENSUS_H_
 
+#include "src/protos/grpc/raft_service.grpc.pb.h"
+
 namespace gfs{
 namespace server{
 
 // This interface aims to extend single-master server to multiple master servers
 // where we obtain consensus using Raft algorithm
 
-class RaftConsessus{
+class RaftServiceImpl final : public protos::grpc::RaftService::Service{
+    public:
+
 
 };
 
