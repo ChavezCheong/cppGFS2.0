e0539eedc1f09b52b124ea00e9b743000d6ee2ba
ngungu46 <ngungu46@Nguyens-MacBook-Pro.local>
2023-03-23 13:29:40 -0400

    Scratch version of AppendEntries implementation

--- src/server/master_server/raft_service_impl.cc
+++ src/server/master_server/raft_service_impl.cc
@@ -49,6 +49,76 @@ grpc::Status AppendEntries(grpc::ServerContext* context,
 
     
     // TODO: implement logic here
+
+    int prev_log_index = request->prevlogindex();
+    int prev_log_term = request->prevlogterm();
+
+
+    // reject the request if leader term is less than current term
+    if(request->term() < currentTerm){
+        // currentTerm = request->term();
+        reply->set_term(currentTerm);
+        reply->set_success(false);
+        return grpc::Status::OK; // might need to change this
+    }
+
+    // TODO: handle election timeout
+
+
+    // If the log doesn't contain an entry at prevLogIndex whose term matches prevLogTerm, reject the request
+
+    if(prev_log_index >= log.size() || log[prev_log_index].term() != prev_log_term){
+        reply->set_term(currentTerm);
+        reply->set_success(false);
+        return grpc::Status::OK; // might need to change this
+    }
+
+    // iterate over entry and append to the log
+
+    for(const auto& entry : request->entries()){
+        int index = entry.index();
+        int term = entry.term();
+
+        // TODO: implement append entries logic here
+        // might need to fix
+
+        if(index < log.size() && log[index].term() != term){
+            // if the entry conflicts with the log, i.e has the same index but different term, delete entries 
+            log.resize(index);
+        }
+
+        if(index >= log.size()){
+            // add log entry to the log, not sure how to do this efficiently
+        }
+
+        // If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
+
+        if(request->leadercommit() > commitIndex){
+            // log.size() - 1 is the index of last entry, might change depend on implementation
+            commitIndex = std::min(request->leadercommit(), log.size() - 1);
+
+        }
+
+
+        return grpc::Status::OK;
+
+
+        // TODO: Leader might send AppendEntries RPC after voting to convert Candidates to Follower
+        // TODO: Follower: If election timeout elapses without receiving AppendEntries
+        // RPC from current leader or granting vote to candidate: convert to candidate
+        // TODO: Follower: Upon election: send initial empty AppendEntries RPCs (heartbeat) to each server; repeat during idle periods to prevent election timeouts 
+        // TODO: If last log index ≥ nextIndex for a follower: send AppendEntries RPC with log entries starting at nextIndex
+        /*• If successful: update nextIndex and matchIndex for
+        follower (§5.3)
+        • If AppendEntries fails because of log inconsistency:
+        decrement nextIndex and retry
+        */
+        
+    }
+
+    // If the leader's commit index is greater than ours, update our commit index
+
+
     return grpc::Status::OK;
 }
 
