c7cc483e6fcd7ef892c671d2a6611df9b2cfecb9
ngungu46 <ngungu46@Nguyens-MacBook-Pro.local>
2023-03-23 02:17:25 -0400

    Add functions in raft_service_impl.cc

--- src/server/master_server/raft_service_impl.cc
+++ src/server/master_server/raft_service_impl.cc
@@ -3,7 +3,24 @@ namespace service{
 
 #include "src/protos/grpc/raft_service.grpc.pb.h"
 
+grpc::Status RequestVote(grpc::ServerContext* context,
+    const protos::grpc::RequestVoteRequest* request,
+    protos::grpc::RequestVoteReply* reply){
 
+    
+    // TODO: implement logic here
+
+    return grpc::Status::OK;
+}
+
+grpc::Status AppendEntries(grpc::ServerContext* context,
+    const protos::grpc::AppendEntriesRequest* request,
+    protos::grpc::AppendEntriesReply* reply){
+
+    
+    // TODO: implement logic here
+    return grpc::Status::OK;
+}
 
 }
 }
