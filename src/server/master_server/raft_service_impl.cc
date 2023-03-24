c4bbb49eb485037c80a653818e1c0c2fb830cbf3
Mark Liu <markyangliu3@gmail.com>
2023-03-24 01:10:06 -0400

    Add initialize

--- src/server/master_server/raft_service_impl.cc
+++ src/server/master_server/raft_service_impl.cc
@@ -7,8 +7,8 @@ using protos::grpc::RequestVoteRequest;
 namespace gfs{
 namespace service{
 
-RaftServiceImpl(common::ConfigManager* config_manager){
-     config_manager_(config_manager);
+void RaftServiceImpl::Initialize(){
+    
 }
 
 
@@ -147,7 +147,7 @@ void RaftServiceImpl::ConvertToFollower(){
 void RaftServiceImpl::ConvertToCandidate(){
     // Once a server is converted to candidate, we increase the current term
     currentTerm++;
-    numVotes = 0;
+
     votedFor = serverId;
 
     reset_election_timeout();
@@ -171,22 +171,8 @@ void RaftServiceImpl::ConvertToCandidate(){
         // send request vote to the server
 
         // SendRequest(request, server);
-
-        // TODO: create a client to communicate with the masters
-
-    }
-
-    // count the votes:
-
-    if(numVotes >= 2){
-        ConvertToLeader();
     }
 }
-
-RaftServiceImpl::State RaftServiceImpl::GetCurrentState(){
-    return currState;
-}
-
 
 void RaftServiceImpl::reset_election_timeout(){
     // TODO: add a Timer here
