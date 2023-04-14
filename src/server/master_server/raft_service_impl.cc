b1f221549155597cc89696fb17daaca83111c9e1
Mark Liu <markyangliu3@gmail.com>
2023-04-13 21:18:35 -0400

    Fix bug with setting nextIndex and added more logs

--- src/server/master_server/raft_service_impl.cc
+++ src/server/master_server/raft_service_impl.cc
@@ -105,7 +105,8 @@ namespace gfs
                 LogEntry empty_entry;
                 empty_entry.set_term(0);
                 empty_entry.set_index(0);
-                log_ = std::vector<LogEntry>(1, empty_entry);
+                log_ = std::vector<LogEntry>(0);
+                log_.push_back(empty_entry);
             }
 
             commitIndex = 0;
@@ -273,7 +274,7 @@ namespace gfs
 
             lock_.Lock();
 
-            LOG(INFO) << "Handle Append Entries RPC from: " << request->leaderid();
+            LOG(INFO) << "Handle Append Entries RPC from: " << request->leaderid() << "Current Log size: " << log_.size();
 
             for (auto log : log_) {
                 LOG(INFO) << log.term() << " and  " << log.open_file().filename();
@@ -337,6 +338,7 @@ namespace gfs
 
                 if (index >= log_.size())
                 {
+                    LOG(INFO) << "pushing it back";
                     log_.push_back(entry);
                     // raft_service_log_manager_->DeleteLogEntries(log_.size());
                 }
@@ -555,7 +557,7 @@ of matchIndex[i] ≥ N, and log[N].term == currentTerm: set commitIndex = N (§5
             // initialize nextIndex and matchIndex
             for (std::string server_name : all_servers)
             {
-                nextIndex[server_name] = log_.size() + 1;
+                nextIndex[server_name] = log_.size();
                 matchIndex[server_name] = 0;
             }
 
@@ -574,7 +576,7 @@ of matchIndex[i] ≥ N, and log[N].term == currentTerm: set commitIndex = N (§5
 
             for (auto server_name : all_servers)
             {
-                LOG(INFO) << "Sending an AppendEntries RPC to server" << server_name;
+                // LOG(INFO) << "Sending an AppendEntries RPC to server" << server_name;
                 append_entries_results.push_back(
                     std::async(std::launch::async, [&, server_name]()
                                {
@@ -608,7 +610,7 @@ of matchIndex[i] ≥ N, and log[N].term == currentTerm: set commitIndex = N (§5
                     {
                         AppendEntriesReply reply = append_entries_reply.value();
                         // TODO: add logic to resend appendentries if needed
-                        LOG(INFO) << "Received AppendEntriesReply " << reply.SerializeAsString();
+                        //LOG(INFO) << "Received AppendEntriesReply " << reply.SerializeAsString();
                     }
                     else
                     {
@@ -636,7 +638,7 @@ of matchIndex[i] ≥ N, and log[N].term == currentTerm: set commitIndex = N (§5
             // term of prevLogIndex entry
                 request.set_prevlogterm(log_[prev_log_index].term());
                 request.set_leadercommit(commitIndex);
-                LOG(INFO) << "non empty log" << prev_log_index << " " << log_[prev_log_index].term() << " " << commitIndex;
+                LOG(INFO) << "non empty log" << prev_log_index << " " << log_[prev_log_index].term() << " " << commitIndex << " " <<  log_.size();
             }
 
             // log entries to store
